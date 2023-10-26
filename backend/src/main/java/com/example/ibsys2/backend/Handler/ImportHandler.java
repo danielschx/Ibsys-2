dd20ea308bd73a74d1750a823466347fe9127b3d
Daniel Schleise <77122105+danielschx@users.noreply.github.com>
2023-10-26 22:24:01 +0200

    Inital ImportController & Handler without Logic

---
+++ backend/src/main/java/com/example/ibsys2/backend/Handler/ImportHandler.java
@@ -0,0 +1,59 @@
+package com.example.ibsys2.backend.Handler;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImportHandler {
+
+    public void parseBody(Map<String, Object> body) {
+        parseForecast((Map<String, Object>) body.get("forecast"));
+        parseWarehousestockArticles((List<Map<String, Object>>) ((Map<String, Object>)body.get("warehousestock")).get("articles"));
+        parseInWardStockMovementOrders((List<Map<String, Object>>)((Map<String, Object>)body.get("inwardstockmovement")).get("order"));
+        parseFutureInWardStockMovementOrders((List<Map<String, Object>>)((Map<String, Object>)body.get("futureinwardstockmovement")).get("order"));
+        parseIdleTimeCostWorkplaces((List<Map<String, Object>>)((Map<String, Object>)body.get("idletimecosts")).get("workplace"));
+        parseWaitingListWorkstations((List<Map<String, Object>>)((Map<String, Object>)body.get("waitinglistworkstations")).get("workplace"));
+        parseWaitingListStock((List<Map<String, Object>>)((Map<String, Object>)body.get("waitingliststock")).get("missingpart"));
+        parseOrdersInWork((List<Map<String, Object>>)((Map<String, Object>)body.get("ordersinwork")).get("workplace"));
+        parseCompletedOrders((List<Map<String, Object>>)((Map<String, Object>)body.get("completeorders")).get("order"));
+        parseCycleTimes((Map<String, Object>)body.get("cycletimes"));
+        parseResult((Map<String, Object>) body.get("result"));
+         
+    }
+
+    private void parseResult(Map<String, Object> map) {
+    }
+
+    private void parseCycleTimes(Map<String, Object> map) {
+    }
+
+    private void parseCompletedOrders(List<Map<String, Object>> list) {
+    }
+
+    private void parseOrdersInWork(List<Map<String, Object>> list) {
+    }
+
+    private void parseWaitingListStock(List<Map<String, Object>> list) {
+    }
+
+    private void parseWaitingListWorkstations(List<Map<String, Object>> list) {
+    }
+
+    private void parseIdleTimeCostWorkplaces(List<Map<String, Object>> list) {
+    }
+
+    private void parseFutureInWardStockMovementOrders(List<Map<String, Object>> list) {
+    }
+
+    private void parseInWardStockMovementOrders(List<Map<String, Object>> list) {
+    }
+
+    private void parseWarehousestockArticles(List<Map<String, Object>> list) {
+    }
+
+    private void parseForecast(Map<String, Object> map) {
+    }
+
+}
---
+++ backend/src/main/java/com/example/ibsys2/backend/controller/ImportController.java
@@ -0,0 +1,32 @@
+package com.example.ibsys2.backend.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ibsys2.backend.Handler.ImportHandler;
+
+/**
+ * RestController für den XML-Import
+ */
+
+@RestController
+@RequestMapping("/api")
+public class ImportController {
+    
+    @Autowired
+    private ImportHandler importHandler;
+
+    @PostMapping("/import")
+    public ResponseEntity parseJson(@RequestBody Map<String, Object> requestBody){
+        System.out.println(requestBody);
+        importHandler.parseBody((Map<String, Object>)requestBody.get("results")); 
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
