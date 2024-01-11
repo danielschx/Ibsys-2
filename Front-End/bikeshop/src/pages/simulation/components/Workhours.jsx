2d44a02c6ded4e696e955d487517e984561ab450
MelanieArndt11 <148140245+MelanieArndt11@users.noreply.github.com>
2024-01-11 13:06:49 +0100

    delete text

--- Front-End/bikeshop/src/pages/simulation/components/Workhours.jsx
+++ Front-End/bikeshop/src/pages/simulation/components/Workhours.jsx
@@ -45,105 +45,103 @@ function Workinghours({ data, calculations, validate }) {
   };
 
   return (
-    <>
-      <Box sx={{ px: 3, display: "flex", flexDirection: "column" }}>
-        <Box sx={{ fontSize: "14px", textAlign: "start", mt:6 }}>
-          <Typography>{t("simulation.workhoursInfo.info1")}</Typography>
-          <br />
-          <Typography>{t("simulation.workhoursInfo.info2")}</Typography>
-          <br />
-          <Typography>{t("simulation.workhoursInfo.info3")}</Typography>
-          <br />
-          <Typography>{t("simulation.workhoursInfo.info4")}</Typography>
-          <br />
-          <Typography>{t("simulation.workhoursInfo.info5")}</Typography>
-          <br />
-        </Box>
-        {items &&
-          items.length > 0 &&
-          items.map((oElement) => {
-            return (
-              <Box
-                key={oElement.station}
-                sx={{
-                  my: 2,
-                  p: 2,
-                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  backgroundColor: "#ffffff",
-                  width: "650px",
-                }}
-              >
-                <Typography
-                  variant="h5"
-                  sx={{ fontWeight: "bold", marginBottom: "1rem" }}
-                >
-                  {t("simulation.workstation")} {oElement.station}:
-                </Typography>
-                <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                    <FormControl fullWidth>
-                      <InputLabel>{t("simulation.shift")}</InputLabel>
-                      <Input
-                        type="number"
-                        id={"inputshift" + oElement.station}
-                        aria-describedby="form-helper"
-                        onInput={(oEvent) => {
-                          // ... (Ihre vorhandenen Eingabehandler)
-                        }}
-                        inputProps={{
-                          min: 0,
-                          max: 3,
-                          onKeyDown: (event) => {
-                            // ... (Ihre vorhandenen Eingabehandler)
-                          },
-                        }}
-                        value={oElement.shift}
-                        onChange={(oEvent) => {
-                          // ... (Ihre vorhandenen Eingabehandler)
-                        }}
-                      />
-                    </FormControl>
-                    {calculations && (
-                      <Typography variant="body2" marginTop="0.5rem">
-                        Produktions- und Rüstzeiten:{" "}
-                        {getCalculationExplanation(oElement.station)}
-                      </Typography>
-                    )}
-                  </Grid>
-                  <Grid item xs={6}>
-                    <FormControl fullWidth>
-                      <InputLabel>{t("simulation.overtime")}</InputLabel>
-                      <Input
-                        id={"inputovertime" + oElement.station}
-                        type="number"
-                        inputProps={{
-                          min: 0,
-                          max: iMaxNumber,
-                          onKeyDown: (event) => {
-                            // ... (Ihre vorhandenen Eingabehandler)
-                          },
-                        }}
-                        defaultValue={oElement.overtime}
-                        onInput={(oEvent) => {
-                          // ... (Ihre vorhandenen Eingabehandler)
-                        }}
-                        onChange={(oEvent) => {
-                          // ... (Ihre vorhandenen Eingabehandler)
-                        }}
-                      />
-                    </FormControl>
-                  </Grid>
-                </Grid>
-              </Box>
-            );
-          })}
-      </Box>
-
-      <Tooltip arrow title={t("simulation.tooltipWorkhours")}>
+    <Box sx={{ px: 3, display: "flex", flexDirection: "column" }}>
+      <Tooltip arrow title={t("simulation.tooltipWorkhours")} sx={{ mt: 2, mb: -1, alignSelf: "end" }}>
         <InfoOutlined />
       </Tooltip>
-    </>
+
+      <Box sx={{ fontSize: "14px", textAlign: "start" }}>
+        <Typography>{t("simulation.workhoursInfo.info1")}</Typography>
+        {/* <br />
+        <Typography>{t("simulation.workhoursInfo.info2")}</Typography>
+        <br />
+        <Typography>{t("simulation.workhoursInfo.info3")}</Typography>
+        <br />
+        <Typography>{t("simulation.workhoursInfo.info4")}</Typography>
+        <br />
+        <Typography>{t("simulation.workhoursInfo.info5")}</Typography>
+        <br /> */}
+      </Box>
+      {items &&
+        items.length > 0 &&
+        items.map((oElement) => {
+          return (
+            <Box
+              key={oElement.station}
+              sx={{
+                my: 2,
+                p: 2,
+                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                borderRadius: "8px",
+                backgroundColor: "#ffffff",
+                width: "650px",
+              }}
+            >
+              <Typography
+                variant="h5"
+                sx={{ fontWeight: "bold", marginBottom: "1rem" }}
+              >
+                {t("simulation.workstation")} {oElement.station}:
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <InputLabel>{t("simulation.shift")}</InputLabel>
+                    <Input
+                      type="number"
+                      id={"inputshift" + oElement.station}
+                      aria-describedby="form-helper"
+                      onInput={(oEvent) => {
+                        // ... (Ihre vorhandenen Eingabehandler)
+                      }}
+                      inputProps={{
+                        min: 0,
+                        max: 3,
+                        onKeyDown: (event) => {
+                          // ... (Ihre vorhandenen Eingabehandler)
+                        },
+                      }}
+                      value={oElement.shift}
+                      onChange={(oEvent) => {
+                        // ... (Ihre vorhandenen Eingabehandler)
+                      }}
+                    />
+                  </FormControl>
+                  {calculations && (
+                    <Typography variant="body2" marginTop="0.5rem">
+                      Produktions- und Rüstzeiten:{" "}
+                      {getCalculationExplanation(oElement.station)}
+                    </Typography>
+                  )}
+                </Grid>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <InputLabel>{t("simulation.overtime")}</InputLabel>
+                    <Input
+                      id={"inputovertime" + oElement.station}
+                      type="number"
+                      inputProps={{
+                        min: 0,
+                        max: iMaxNumber,
+                        onKeyDown: (event) => {
+                          // ... (Ihre vorhandenen Eingabehandler)
+                        },
+                      }}
+                      defaultValue={oElement.overtime}
+                      onInput={(oEvent) => {
+                        // ... (Ihre vorhandenen Eingabehandler)
+                      }}
+                      onChange={(oEvent) => {
+                        // ... (Ihre vorhandenen Eingabehandler)
+                      }}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Box>
+          );
+        })}
+    </Box>
   );
 }
 
